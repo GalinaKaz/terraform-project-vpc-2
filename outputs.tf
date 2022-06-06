39ad866aaed58b7f95113a08b8270b97b2965a5f
galina kaz <galina.kaz@gmail.com>
2022-06-06 12:41:33 +0300

    working version 2

--- outputs.tf
+++ outputs.tf
@@ -10,37 +10,3 @@ output "public_WEB_server_name" {
   value       = [aws_instance.web-server.public_dns]
 }
 
-#---------------------additionals---------------------------
-
-
-/*output "EndPoint" {
-  description = "End Point to web Server"
-  value       = [aws_db_instance.RdsForVpcProject.endpoint]
-}
-
-output "vpc_id" {
-  description = "ID of project VPC"
-  value       = module.vpc.vpc_id
-}
-output "name" {
-  description = "name"
-  value       = module.vpc.name
-}
-
-output "aws_db_subnet_group" {
-  description = "db subnet name"
-  value       = aws_db_subnet_group.for-database.name
-}
-
-output "private_subnets" {
-  description = "private subnets"
-  value       = [module.vpc.private_subnets]
-}
-
-
-output "public_subnets" {
-  description = "public subnets"
-  value       = flatten([module.vpc.public_subnets])
-}
-*/
-
